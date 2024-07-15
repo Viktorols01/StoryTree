0b817ad34363cf51cd49b572d4b86115f56c5bfc
Viktor Ols <viktorols01@gmail.com>
2024-07-15 21:29:44 +0200

    Finalize the major refactoring of code for now

--- main/ReaderProgram.java
+++ main/ReaderProgram.java
@@ -6,7 +6,7 @@ import tools.FileHandler;
 
 class ReaderProgram {
     public static void main(String[] args) {
-        StoryNode root = FileHandler.loadObject("files/stories", "Stories", ".story");
+        StoryNode root = FileHandler.loadObject("files/stories", "Story files", "story");
 
         if (root != null) {
             ConsoleStoryReader reader = new ConsoleStoryReader(root);
